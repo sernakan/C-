cea9b78caf99083fcc7b3273ef76071d1d3db5d8
Natalya <can-natasha@yzndex.ru>
2023-03-03 23:53:00 +0500

    Sum odd

---
+++ cube/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+//Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N
+
+//3 - 1 , 8, 27
+//5 - 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+string res = $"{N} - ";
+for (int i = 1; i <= N; i++)
+{
+    int cube = i * i * i;
+    res += $"{cube}, ";
+}
+Console.WriteLine(res);
---
+++ сумма нечетных/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100,100);
+}
+void FindSums(int [] array )
+{
+    int Sumodd  = 0; 
+    for (int i=0; i< array.Length; i++)
+    if (i % 2 != 0)
+    Sumodd += array [i]; 
+    Console.WriteLine($" -> {Sumodd}");
+
+}
+void PrintArray(int[] array)
+{
+    foreach (var el in array)
+        Console.Write($"{el} ");
+}
+int[] array = new int[4];
+FillArray(array);
+PrintArray(array);
+FindSums(array);
+
